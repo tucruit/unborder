21d28258b12a8e243f514488a911dc8bf6a45cb8
kato <kato@e-catchup.jp>
2023-05-24 17:40:15 +0900

    パンくずが漏れていたので反映

--- htdocs/theme/instant-page/Elements/crumbs.php
+++ htdocs/theme/instant-page/Elements/crumbs.php
@@ -5,7 +5,7 @@
  * ページタイトルが直属のカテゴリ名と同じ場合は、直属のカテゴリ名を省略する
  */
 if (!isset($separator)) {
-	$separator = '&nbsp;&gt;&nbsp;';
+	$separator = '';
 }
 if (!isset($home)) {
 	$home = __d('baser', 'トップページ');
@@ -13,13 +13,17 @@ if (!isset($home)) {
 $crumbs = $this->BcBaser->getCrumbs();
 if (!empty($crumbs)) {
 	foreach($crumbs as $key => $crumb) {
-		if ($this->BcArray->last($crumbs, $key)) {
-			if ($this->viewPath != 'home' && $crumb['name']) {
-				$this->BcBaser->addCrumb(h($crumb['name']));
+		// categoryページ以外を出力
+		if(!strpos($crumb['url'],'/archives/category/')) {
+			if ($this->BcArray->last($crumbs, $key)) {
+				if ($this->viewPath != 'home' && $crumb['name']) {
+					$this->BcBaser->addCrumb(h($crumb['name']));
+				}
+			} else {
+				$this->BcBaser->addCrumb(h($crumb['name']), $crumb['url']);
 			}
-		} else {
-			$this->BcBaser->addCrumb(h($crumb['name']), $crumb['url']);
 		}
+
 	}
 } elseif (empty($crumbs)) {
 	if ($this->name == 'CakeError') {
@@ -27,12 +31,28 @@ if (!empty($crumbs)) {
 	}
 }
 ?>
-<!-- BREAD CRUMBS -->
-<div class="sub-breadcrumbs">
-	<div class="l-subContentsContainer sub-breadcrumbsInner">
-		<ol class="sub-breadcrumbs-list">
-			<?php $this->BcBaser->crumbs('', $home, true) ?>
-		</ol>
+
+<?php if (empty($onSchema)): ?>
+	<?php
+	if ($this->BcBaser->isHome()) {
+		echo $home;
+	} else {
+		$this->BcBaser->crumbs($separator, $home);
+	}
+	?>
+<?php else: ?>
+	<div class="sub-breadcrumbs">
+		<div class="l-subContentsContainer sub-breadcrumbsInner">
+			<ol class="sub-breadcrumbs-list" itemscope itemtype="http://schema.org/BreadcrumbList">
+				<?php if ($this->BcBaser->isHome()): ?>
+					<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><span
+							itemprop="name"><?php echo $home ?></span>
+						<meta itemprop="position" content="1"/>
+					</li>
+				<?php else: ?>
+					<?php $this->BcBaser->crumbs($separator, $home, true) ?>
+				<?php endif ?>
+			</ol>
+		</div>
 	</div>
-</div>
-<!-- /BREAD CRUMBS -->
+<?php endif ?>
