014e6aa2dad28972fe689a165c35a10fc8c36d79
kato <kato@e-catchup.jp>
2023-05-30 18:35:44 +0900

    ログインページはフロント側で統一

--- htdocs/theme/instant-page/Users/admin/login.php
+++ htdocs/theme/instant-page/Users/admin/login.php
@@ -5,14 +5,14 @@ if (!empty($server['HTTP_REFERER']) && strpos($server['HTTP_REFERER'], 'admin/in
 	header('Location: '. $this->BcBaser->getUri('/mypage/instant_page/instant_page_users/login'));
 	exit;
 } else {
-	$enableIp = [
-		//'::1',
-		'183.76.75.203',
-		'118.27.108.63',
-	];
-	if (in_array($server['REMOTE_ADDR'], $enableIp)) {
-		include __DIR__ . DS . '../../../admin-third/Users/admin/login.php';
-	} else {
+	// $enableIp = [
+	// 	//'::1',
+	// 	'183.76.75.203',
+	// 	'118.27.108.63',
+	// ];
+	// if (in_array($server['REMOTE_ADDR'], $enableIp)) {
+	// 	include __DIR__ . DS . '../../../admin-third/Users/admin/login.php';
+	// } else {
 		include __DIR__ . DS . '../../../../app/Plugin/InstantPage/View/InstantPageUsers/admin/login.php';
-	}
+	// }
 }
