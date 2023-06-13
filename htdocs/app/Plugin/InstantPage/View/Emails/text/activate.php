0a62e9c5fbf215e41901f99a89e4d4b260601f1c
kato <kato@e-catchup.jp>
2023-06-13 15:51:06 +0900

    メールメッセージの調整

--- htdocs/app/Plugin/InstantPage/View/Emails/text/activate.php
+++ htdocs/app/Plugin/InstantPage/View/Emails/text/activate.php
@@ -6,7 +6,7 @@
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 担当者：<?php if ($InstantPage['User']['real_name_1']) { echo $InstantPage['User']['real_name_1']; } ?> <?php if ($InstantPage['User']['real_name_2']) { echo $InstantPage['User']['real_name_2']; }?> 様
 
-管理画面URL： <?php echo 'http://'.$_SERVER["HTTP_HOST"].'/cmsadmin/instant_page/instant_page_users/edit/' . $InstantPage['User']['id']; ?>
+管理画面URL： <?php echo 'http://'.$_SERVER["HTTP_HOST"].'/cmsadmin/users/edit/' . $InstantPage['User']['id']; ?>
 
 <?php else: ?>
 
