329d37c496c232b252d8735728e3c457485da5d9
kato <kato@e-catchup.jp>
2023-06-16 23:29:57 +0900

    テーマを一旦空にする

--- htdocs/theme/default_gray/InstantPages/detail.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * インスタントページ公開テーマ
- */
-$this->BcBaser->setTitle(strip_tags($data['InstantPage']['title']));
-$this->BcBaser->setDescription(strip_tags($data['InstantPage']['page_description']));
-$this->BcBaser->setKeywords(strip_tags($data['InstantPage']['page_key_word']));
-
-echo $data['InstantPage']['contents'];
