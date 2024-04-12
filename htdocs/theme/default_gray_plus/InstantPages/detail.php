8dea9f9741992a650eb42acc0a264cba269a77e6
goichi <g.maniwa@hiniarata.jp>
2024-04-12 15:40:43 +0900

    デフォルトテーマをコピーして実験用のテーマを作った

--- htdocs/theme/default_gray_plus/InstantPages/detail.php
+++ htdocs/theme/default_gray_plus/InstantPages/detail.php
@@ -5,5 +5,10 @@
 $this->BcBaser->setTitle(strip_tags($data['InstantPage']['title']));
 $this->BcBaser->setDescription(strip_tags($data['InstantPage']['page_description']));
 $this->BcBaser->setKeywords(strip_tags($data['InstantPage']['page_key_word']));
+?>
 
+<div class="l-container">
+<?php
 echo $data['InstantPage']['contents'];
+?>
+</div>
