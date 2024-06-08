9dd2dce3f749289b2def248e9882e85d15db6b22
Aiko Tabata <tabata@unborder.co.jp>
2024-06-08 15:43:34 +0900

    画面スライドイン

---
+++ htdocs/app/Plugin/BurgerEditor/Addon/block/slidein-img/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * BurgerEditor <baserCMS plugin>
+ *
+ * @copyright		Copyright 2013 -, D-ZERO Co.,LTD.
+ * @link			https://www.d-zero.co.jp/
+ * @package			burger_editor
+ * @since			Baser v 3.0.0
+ * @license			https://market.basercms.net/files/baser_market_license.pdf
+ */
+?>
+<div class="slidein_img">
+    <?php $this->BurgerEditor->type('ckeditor') ?>
+</div>
+
