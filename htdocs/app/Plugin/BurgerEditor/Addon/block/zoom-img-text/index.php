5f56f531b9d3bd7eea18bffc954b11d9893977b6
Aiko Tabata <tabata@unborder.co.jp>
2024-06-13 18:30:59 +0900

    ズーム画像

---
+++ htdocs/app/Plugin/BurgerEditor/Addon/block/zoom-img-text/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * BurgerEditor <baserCMS plugin>
+ *
+ * @copyright		Copyright 2013 -, D-ZERO Co.,LTD.
+ * @link			https://www.d-zero.co.jp/
+ * @package			burger_editor
+ * @since			Baser v 3.0.0
+ * @license			https://market.basercms.net/files/baser_market_license.pdf
+ */
+?>
+<div class="zoom_main">
+    <?php $this->BurgerEditor->type('title-transparent') ?>
+    <div class="zoom_img">
+        <?php $this->BurgerEditor->type('image') ?>
+        <?php $this->BurgerEditor->type('image') ?>
+        <?php $this->BurgerEditor->type('image') ?>
+    </div>
+</div>
